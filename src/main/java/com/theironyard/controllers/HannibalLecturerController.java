6ff24d7d58359e3b9b3badb59bb3ab31ab3738be
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-30 13:11:07 -0400

    initial import

---
+++ src/main/java/com/theironyard/controllers/HannibalLecturerController.java
@@ -0,0 +1,7 @@
+package com.theironyard.controllers;
+
+/**
+ * Created by michaeldelli-gatti on 6/30/16.
+ */
+public class HannibalLecturerController {
+}
---
+++ src/main/java/com/theironyard/services/LecturerRepository.java
@@ -0,0 +1,7 @@
+package com.theironyard.services;
+
+/**
+ * Created by michaeldelli-gatti on 6/30/16.
+ */
+public interface LecturerRepository {
+}
